4758a952c76b8fc907018e141fc9edf7251725de
jam82 <jam@kabelmail.net>
2020-04-25 03:35:03 +0200

    dc role passing

--- molecule/resources/tests/test_dc.py
+++ molecule/resources/tests/test_dc.py
@@ -21,29 +21,34 @@ def test_dns_ldap(host, get_vars):
         assert '_ldap._tcp.' + get_vars['samba_domain'] + ' has SRV' in output
 
 
-# def test_dns_kerberos(host):
-#     test = 'host -t SRV _kerberos._udp.ad.homebase.localdomain.'
-#     output = host.check_output(test)
-#     assert '_kerberos._udp.ad.homebase.localdomain has SRV' in output
+def test_dns_kerberos(host, get_vars):
+    if get_vars['samba_role'] == 'domain controller':
+        test = 'host -t SRV _kerberos._udp.' + get_vars['samba_domain'] + '.'
+        output = host.check_output(test)
+        assert '_kerberos._udp.' + get_vars['samba_domain'] + \
+            ' has SRV' in output
 
 
-# def test_dns_a_record(host):
-#     hostname = host.check_output('hostname -s')
-#     test = "host -t A " + hostname \
-#         + ".ad.homebase.localdomain."
-#     output = host.check_output(test)
-#     assert hostname \
-#         + ".ad.homebase.localdomain has address" in output
+def test_dns_a_record(host, get_vars):
+    if get_vars['samba_role'] == 'domain controller':
+        hostname = host.check_output('hostname -s')
+        test = "host -t A " + hostname + '.' \
+            + get_vars['samba_domain'] + '.'
+        output = host.check_output(test)
+        assert hostname + '.' \
+            + get_vars['samba_domain'] + ' has address' in output
 
 
-# def test_reverse_dns(host):
-#     facts = host.ansible("setup")["ansible_facts"]
-#     ip = facts["ansible_default_ipv4"]["address"]
-#     test = "nslookup " + ip
-#     output = host.check_output(test)
-#     assert "ad.homebase.localdomain" in output
+def test_reverse_dns(host, get_vars):
+    if get_vars['samba_role'] == 'domain controller':
+        facts = host.ansible("setup")["ansible_facts"]
+        ip = facts["ansible_default_ipv4"]["address"]
+        test = "nslookup " + ip
+        output = host.check_output(test)
+        assert get_vars['samba_domain'] in output
 
 
-# def test_dns(host):
-#     google = host.addr("google.com")
-#     assert google.is_resolvable
+def test_dns(host, get_vars):
+    if get_vars['samba_role'] == 'domain controller':
+        google = host.addr("google.com")
+        assert google.is_resolvable
